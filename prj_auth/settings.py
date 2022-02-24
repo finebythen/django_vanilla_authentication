f1db36193f7a0a7b8cf05158537611b0c93d4741
Finn Then <71402477+finebythen@users.noreply.github.com>
2022-02-24 14:00:49 +0100

    Update settings.py

--- prj_auth/settings.py
+++ prj_auth/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_a@wlc54#syf4f8m_s^ycq0%^#=c54$nqboc&f8-w5dea3-72&'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
